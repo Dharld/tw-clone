adaeab81bcd5ff09f9a40e12734012cfc40ed824
Yann djoumessi <yanndjoumessi@gmail.com>
2023-01-24 11:15:44 +0100

    First commit

---
+++ twitter-clone/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthenticationModule } from './features/authentication/authentication.module';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    AuthenticationModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
