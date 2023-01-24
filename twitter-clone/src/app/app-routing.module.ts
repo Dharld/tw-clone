ade54859238fe61a853c960d9bd8405f38df48a9
Yann djoumessi <yanndjoumessi@gmail.com>
2023-01-24 15:01:23 +0100

    Signup page created

--- twitter-clone/src/app/app-routing.module.ts
+++ twitter-clone/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationComponent } from './features/authentication/authentication.component';
+import { SigninComponent } from './features/authentication/pages/signin/signin.component';
+import { SignupComponent } from './features/authentication/pages/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthenticationComponent,
+    children: [
+      {
+        path: 'signup',
+        component: SignupComponent,
+      },
+      {
+        path: 'signin',
+        component: SigninComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- twitter-clone/src/app/features/authentication/authentication.module.ts
+++ twitter-clone/src/app/features/authentication/authentication.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationComponent } from './authentication.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { RouterModule } from '@angular/router';
+import { SigninComponent } from './pages/signin/signin.component';
 
 @NgModule({
-  declarations: [AuthenticationComponent],
-  imports: [CommonModule],
+  declarations: [AuthenticationComponent, SignupComponent, SigninComponent],
+  imports: [CommonModule, RouterModule],
   exports: [AuthenticationComponent],
 })
 export class AuthenticationModule {}
