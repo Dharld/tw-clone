429b076d5ba3a130652f932613223b06d507c9f9
Yann djoumessi <yanndjoumessi@gmail.com>
2023-02-07 19:03:37 +0100

    Sidebar of the homepage

--- twitter-clone/src/app/features/home/home/home.component.ts
+++ twitter-clone/src/app/features/home/home/home.component.ts
@@ -8,6 +8,49 @@ import { AuthService } from 'src/app/core/services/auth.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  routes = [
+    {
+      iconPath: '/assets/icons/icons.svg#homeIcon',
+      href: 'home',
+      active: true,
+    },
+    {
+      iconPath: '/assets/icons/icons.svg#exploreIcon',
+      href: 'explore',
+      active: false,
+    },
+    {
+      iconPath: '/assets/icons/icons.svg#notificationIcon',
+      href: 'notifications',
+      active: false,
+    },
+    {
+      iconPath: '/assets/icons/icons.svg#messageIcon',
+      href: 'message',
+      active: false,
+    },
+    {
+      iconPath: '/assets/icons/icons.svg#bookmarkIcon',
+      href: 'bookmark',
+      active: false,
+    },
+    {
+      iconPath: '/assets/icons/icons.svg#listIcon',
+      href: 'list',
+      active: false,
+    },
+    {
+      iconPath: '/assets/icons/icons.svg#profileIcon',
+      href: 'profile',
+      active: false,
+    },
+    {
+      iconPath: '/assets/icons/icons.svg#moreIcon',
+      href: 'more',
+      active: false,
+    },
+  ];
+
   constructor(public authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
